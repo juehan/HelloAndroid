e28d2099988df8cbc5cb103979eca19bb6993a8a
Albatross <johnny-lee@users.sourceforge.net>
2011-07-21 23:22:26 +1000

    첫번째 커밋.

---
+++ src/com/kossie/app/MyFirstAndroidAppActivity.java
@@ -0,0 +1,27 @@
+package com.kossie.app;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MyFirstAndroidAppActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    
+        Button b = (Button) findViewById(R.id.button1);
+        b.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				startActivity(new Intent(MyFirstAndroidAppActivity.this, SecondActivity.class));
+			}
+		});
+    }
+}
