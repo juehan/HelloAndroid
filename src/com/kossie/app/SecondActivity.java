3752141383fa312e9ca6c00e8c69579fe39d0fe9
Albatross <johnny-lee@users.sourceforge.net>
2011-07-23 01:58:34 +1000

    두번째 커밋 - 이미지 디스플레이 기능 추가
    Signed-off-by: Albatross <johnny-lee@users.sourceforge.net>

--- src/com/kossie/app/SecondActivity.java
+++ src/com/kossie/app/SecondActivity.java
@@ -1,17 +1,27 @@
 package com.kossie.app;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SecondActivity extends Activity {
 	
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.second);
+		
+		Button b2 = (Button) findViewById(R.id.Button1);
+		b2.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				startActivity(new Intent(SecondActivity.this, ThirdActivity.class));	
+			}
+		});
 	}
-	
-	
-
 }
---
+++ src/com/kossie/app/ThirdActivity.java
@@ -0,0 +1,17 @@
+package com.kossie.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class ThirdActivity extends Activity {
+
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.third);
+		
+		ImageView iv = (ImageView)findViewById(R.id.imageView1);
+		iv.setImageResource(R.drawable.bea);
+		
+	}
+}
